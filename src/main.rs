f172ff92c3018f79c8ea3385f07a9da346722d8b
Artur Rafael da Silveira <arturrsilveira85@gmail.com>
2026-07-13 20:41:47 -0300

    Accept First Connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1:8080";
+
+    let listener = TcpListener::bind(address)
+        .expect("Failed to bind Harbor to the address");
+
+    println!("Harbor is listening on http://{address}");
+
+    let _connection = listener
+        .accept()
+        .expect("Failed to accept the incoming connection");
+
+    println!("Harbor accepted its first connection.");
 }
